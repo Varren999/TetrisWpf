53ffbf9b94b2c9ba71956993e6093f4ea76b1808
Varren999 <varren999@gmail.com>
2025-05-04 00:41:52 +0200

    add dapper

---
+++ TetrisWpf/DAL/ConnectDB.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Data.Sqlite;
+using Dapper;
+using System.Collections.Generic;
+using System.IO;
+using Log;
+using System;
+
+namespace TetrisWpf
+{
+    class ConnectDB
+    {
+
+        private readonly SqliteConnection _connection;
+
+        public ConnectDB(string path)
+        {
+            try
+            {
+                if (!File.Exists(path))
+                {
+                    File.Create(path).Close();
+                    using (var connection = new SqliteConnection($"Data Source = {path}"))
+                    {
+                        connection.Open();
+                        var sql = "CREATE TABLE table_score(id INTEGER NOT NULL UNIQUE PRIMARY KEY AUTOINCREMENT, player TEXT NOT NULL, score INTEGER NOT NULL)";
+                        if (connection.Execute(sql) > 0)
+                            Logger.Info($"{path} создана");
+                        else
+                            throw new Exception("Ошибка при создании базы данных");
+                    }
+                }
+
+                _connection = new SqliteConnection($"Data Source = {path}");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex.TargetSite + ex.Message);
+            }
+        }
+
+        public IEnumerable<Scores> GetAll()
+        {
+            const string sql = "SELECT id, player, score FROM table_score ORDER BY score DESC";
+            return _connection.Query<Scores>(sql);
+        }
+
+        public bool Add(Scores score)
+        {
+            var sql = "INSERT INTO table_score(player, score) VALUES (@Player, @Score)";
+            var result = _connection.Execute(sql, new { score.Player, score.Score });
+            return result > 0;
+        }
+    }
+}
---
+++ TetrisWpf/DAL/Scopes.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System;
+
+namespace TetrisWpf
+{
+    class Scores
+    {
+        public int Id { get; set; }
+        public string Player { get; set; }
+        public int Score { get; set; }
+
+        public Scores() { }
+        public Scores(int id, string player, int score) 
+        {
+            Id = id;
+            Player = player;
+            Score = score;
+        }
+
+        public Scores(string player, int score)
+        {
+            Player = player;
+            Score = score;
+        }
+
+        public override string ToString() => $"{Player}\t\t\t{Score}";
+
+    }
+}
